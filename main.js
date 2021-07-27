9e1fc5adc675c24c36dc9174ede4ca15f0466563
Fire-op <asdsaddsuuh@mail.ru>
2021-07-27 14:06:04 +0500

    save

---
+++ main.js
@@ -0,0 +1,28 @@
+var Name = prompt('Введите ваше имя');
+var age  = +prompt('Введите ваш возраст');
+var primer1 = +prompt('6 + 8 = ?');
+var primer2 = +prompt('6 - 5 = ?');
+var primer3 = +prompt('8 * 2 = ?');
+var primer4 = +prompt('14 / 18 = ?');
+var primer5 = +prompt('18 % 12 = ?');
+
+
+alert('Зайдите в консоль')
+
+var asprimer1 = 6 + 8;
+var asprimer2 = 6 - 5;
+var asprimer3 = 8 * 2;
+var asprimer4 = 14 / 18;
+var asprimer5 = 18 % 12
+
+
+
+
+
+console.log('6 + 8 = ' +asprimer1 + ' (Ваш ответ: ' +primer1 + ')');
+console.log('6 - 5 = ' +asprimer2 + ' (Ваш ответ: ' +primer2 + ')');
+console.log('8 * 2 = ' +asprimer3 + ' (Ваш ответ: ' +primer3 + ')');
+console.log('14 / 18 = ' +asprimer4 + ' (Ваш ответ: ' +primer4 + ')');
+console.log('18 % 12 = ' +asprimer5 + ' (Ваш ответ: ' +primer5 + ')');
+
+
